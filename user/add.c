0a0708f5d13aa958d4c5d6fbae185d70dd53403e
githecko <ahmed17503aa@gmail.com>
2025-11-21 22:33:04 +0200

    added usage function for the add command

--- user/add.c
+++ user/add.c
@@ -6,14 +6,20 @@
 
 int main(int argc, char *argv[])
 {
-  if(argc < 4){
-    int num1 = atoi(argv[1]);
-    int num2 = atoi(argv[2]);
-    int sum = num1 + num2;
-    printf("sum = %d\n", sum);
-  } else {
-    printf("error can only take 2 integers \n");
+  if(argc == 2 && argv[1][0] == '?'){
+    printf("Usage: add number1 number2\n");
     exit(0);
   }
+
+  if(argc != 3){
+    printf("error can only take 2 integers \n");
+    exit(1);
+  }
+
+  int num1 = atoi(argv[1]);
+  int num2 = atoi(argv[2]);
+  int sum = num1 + num2;
+  printf("sum = %d\n", sum);
+
   exit(0);
 }
